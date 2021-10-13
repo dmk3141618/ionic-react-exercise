f04ad59c95fa9522133e9a6a15ea7fecfbba4755
dmk3141618 <dmk3141618@gmail.com>
2021-10-13 15:01:25 +0900

    feat: Displaying Photos

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -7,7 +7,7 @@ import { camera, trash, close } from 'ionicons/icons';
 import { usePhotoGallery } from "../hooks/usePhotoGallery";
 
 const Tab2: React.FC = () => {
-  const { takePhoto } = usePhotoGallery();
+  const { photos, takePhoto } = usePhotoGallery();
 
   return (
     <IonPage>
@@ -17,6 +17,15 @@ const Tab2: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
+        <IonGrid>
+          <IonRow>
+            {photos.map((photo, index) => (
+              <IonCol size="6" key={index}>
+                <IonImg src={photo.webviewPath} />
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
         <IonFab vertical="bottom" horizontal="center" slot="fixed">
           <IonFabButton onClick={() => takePhoto()}>
             <IonIcon icon={camera}></IonIcon>
